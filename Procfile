ed15c79d6f0eb4eb365ea7bd8356f28ee1cb1481
PMHStudio <pmhstudio.pmh@gmail.com>
2019-03-16 21:35:53 +0900

    Die Bot init

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: node diebot.js
---
+++ diebot.js
@@ -0,0 +1,53 @@
+const discord = require('discord.js')
+const client = new discord.Client()
+const Heroku = require('heroku-client')
+const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN })
+client.login(process.env.Token)
+
+client.on('message', (message) => {
+  if (message.author === client.user) return
+  if (message.content === '뮤다이') {
+    heroku.get('https://api.heroku.com/apps/mubotapi/dynos/worker.1').then((dyno) => {
+      if (dyno.state === 'up') {
+        message.channel.send('ㄴㄴ, 뮤봇 살아있음')
+      } else {
+        message.channel.send('뮤다이')
+      }
+    }).catch(() => {
+      message.channel.send('뮤다이')
+    })
+  }
+  if (message.content === 'cq다이') {
+    heroku.get('https://api.heroku.com/apps/codequizapi/dynos/worker.1').then((dyno) => {
+      if (dyno.state === 'up') {
+        message.channel.send('ㄴㄴ, cq 살아있음')
+      } else {
+        message.channel.send('cq다이')
+      }
+    }).catch(() => {
+      message.channel.send('cq다이')
+    })
+  }
+  if (message.content === '카텐다이') {
+    heroku.get('https://api.heroku.com/apps/keeptypingandnobodyexplodes/dynos/worker.1').then((dyno) => {
+      if (dyno.state === 'up') {
+        message.channel.send('ㄴㄴ, 카텐 살아있음')
+      } else {
+        message.channel.send('카텐다이')
+      }
+    }).catch(() => {
+      message.channel.send('카텐다이')
+    })
+  }
+  if (message.content === '찻봇다이') {
+    heroku.get('https://api.heroku.com/apps/chartbotapi/dynos/worker.1').then((dyno) => {
+      if (dyno.state === 'up') {
+        message.channel.send('ㄴㄴ, 찻봇 살아있음')
+      } else {
+        message.channel.send('찻봇다이')
+      }
+    }).catch(() => {
+      message.channel.send('찻봇다이')
+    })
+  }
+})
