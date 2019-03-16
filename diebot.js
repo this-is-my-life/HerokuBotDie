4a4dd90ff79aa5f649bd5c3a5d0050ad52e55b77
PMHStudio <pmhstudio.pmh@gmail.com>
2019-03-16 22:14:45 +0900

    Move to Array Style

--- diebot.js
+++ diebot.js
@@ -4,67 +4,55 @@ const Heroku = require('heroku-client')
 const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN })
 client.login(process.env.Token)
 let result
-let die
-let live
+let die = ['좋은일하다 죽었음.', '갑분뒤짐', '내 죽음을 알리지말라...']
+let live = ['ㄴㄴ, 살아있음', '살아있단깨 나는잘 살아있당께', '나는 살아있다!!!!']
 
 client.on('message', (message) => {
   result = Math.floor(Math.random() * 3) + 1
-  if (result === 1) {
-      live = 'ㄴㄴ, 살아있음'
-      die = '좋은일하다 죽었음.'
-  } 
-  if (result === 2) {
-      live = '살아있당깨 나는잘 살아있당께'
-      die = '갑분뒤짐....'
-  } 
-  if (result === 3) {
-      live = '나는 살아있다!!!!'
-      die = '내 죽음을 알리지말라....'
-  } 
 
   if (message.author === client.user) return
   if (message.content === '뮤다이') {
     heroku.get('https://api.heroku.com/apps/mubotapi/dynos/worker.1').then((dyno) => {
       if (dyno.state === 'up') {
-        message.channel.send(live)
+        message.channel.send(live[result])
       } else {
-        message.channel.send(die)
+        message.channel.send(die[result])
       }
     }).catch(() => {
-      message.channel.send(die)
+      message.channel.send(die[result])
     })
   }
   if (message.content === 'cq다이') {
     heroku.get('https://api.heroku.com/apps/codequizapi/dynos/worker.1').then((dyno) => {
       if (dyno.state === 'up') {
-        message.channel.send(live)
+        message.channel.send(live[result])
       } else {
-        message.channel.send(die)
+        message.channel.send(die[result])
       }
     }).catch(() => {
-      message.channel.send(die)
+      message.channel.send(die[result])
     })
   }
   if (message.content === '카텐다이') {
     heroku.get('https://api.heroku.com/apps/keeptypingandnobodyexplodes/dynos/worker.1').then((dyno) => {
       if (dyno.state === 'up') {
-        message.channel.send(live)
+        message.channel.send(live[result])
       } else {
-        message.channel.send(die)
+        message.channel.send(die[result])
       }
     }).catch(() => {
-      message.channel.send(die)
+      message.channel.send(die[result])
     })
   }
   if (message.content === '찻봇다이') {
     heroku.get('https://api.heroku.com/apps/chartbotapi/dynos/worker.1').then((dyno) => {
       if (dyno.state === 'up') {
-        message.channel.send(live)
+        message.channel.send(live[result])
       } else {
-        message.channel.send(die)
+        message.channel.send(die[result])
       }
     }).catch(() => {
-      message.channel.send(die)
+      message.channel.send(die[result])
     })
   }
   if (message.content === '봇다이다이') {
